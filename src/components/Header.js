3f26d05f66251fc60431bcc521e72f39881b8958
Johannes Schilling <jsh.schilling@fau.de>
2021-12-26 23:38:06 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import Button from "./Button.js";
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Klick");
+  };
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button onClick={onClick} color="green" text="hello1"></Button>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+//Prop Types
+// Header.propTypes = {
+//     title: PropTypes.string,
+// }
+
+//CSS Styling
+// const headingStyle = {
+//     color:'red'
+// }
+
+export default Header;
