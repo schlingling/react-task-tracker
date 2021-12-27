1ffae8c15d4f40aea5e9d29876b677614ba31a43
Johannes Schilling <jsh.schilling@fau.de>
2021-12-27 15:02:46 +0100

    added changes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,83 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 4,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-    {
-      id: 5,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+
+    getTasks();
+  }, []); //Second argument is, when fetching depend on other values
+
+  //Fetch Tasks from REST API
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:1200/tasks");
+    const data = await res.json();
+    return data;
+  };
+
+  //Fetch Task from REST API
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:1200/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
 
   //Add Task
-  const addTask = (task) => {
-    console.log(task);
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:1200/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
+
+    // console.log(task);
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
   };
 
   //Delete Task
-  const deleteTask = (id) => {
-    console.log("delete", id);
-    setTasks(tasks.filter((task) => task.id !== id));
+  const deleteTask = async (ID) => {
+    console.log("delete", ID);
+    await fetch(`http://localhost:1200/tasks/${ID}`, {
+      method: "DELETE",
+    });
+    setTasks(tasks.filter((task) => task.id !== ID));
   };
 
   //Toggle Reminder
-  const toggleReminder = (id) => {
-    console.log(id);
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:1200/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
@@ -78,6 +98,7 @@ const App = () => {
       ) : (
         "No Tasks to show"
       )}{" "}
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <Footer>
+      <p>Copyright &copy; 2021</p>
+      <a href="/about">About</a>
+    </Footer>
+  );
+};
+export default Footer;
