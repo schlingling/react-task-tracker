2ba442e99c59d57a938f6d0ceda30fd528301efd
Johannes Schilling <jsh.schilling@fau.de>
2021-12-27 12:41:52 +0100

    add form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 import Button from "./Button.js";
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("Klick");
-  };
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button onClick={onClick} color="green" text="Add"></Button>
+      <Button
+        onClick={onAdd}
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+      ></Button>
     </header>
   );
 };
